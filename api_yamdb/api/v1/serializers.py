fa5ca5ab683753f1e7a0c89f1d754ef62b3e3771
Герман Кабачков <german_kabachkov@mail.ru>
2022-10-28 14:24:25 +0300

    edited for pep8

--- api_yamdb/api/v1/serializers.py
+++ api_yamdb/api/v1/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 from rest_framework.validators import UniqueTogetherValidator
-
 from reviews.models import Category, Comment, Genre, Review, Title
 from users.models import User
 
