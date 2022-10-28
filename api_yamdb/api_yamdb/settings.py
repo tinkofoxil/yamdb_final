cf66e9c53a47d8dd7948b3a498604abd52787cc2
Герман Кабачков <german_kabachkov@mail.ru>
2022-10-28 14:53:09 +0300

    edited workflow

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -1,8 +1,9 @@
 import os
-from datetime import timedelta
-from dotenv import load_dotenv, find_dotenv
 
-load_dotenv(find_dotenv())
+import dotenv
+from datetime import timedelta
+
+dotenv.load_dotenv(dotenv.find_dotenv())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
