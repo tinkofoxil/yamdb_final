17ac9675737c4913ff52ffebaa5172ac7201444b
Герман Кабачков <german_kabachkov@mail.ru>
2022-11-09 17:44:12 +0300

    update workflow and settings

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -5,8 +5,6 @@ from datetime import timedelta
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ['SECRET_KEY']
-
 SECRET_KEY = os.getenv('SECRET_KEY', default='secret')
 
 # SECURITY WARNING: don't run with debug turned on in production!
